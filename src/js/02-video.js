e020ece69e12b4055fbcebda45de44102c819507
Ilja Keldanovich <ilja.keld@gmail.com>
2022-05-09 21:16:56 +0200

    Task 2 in progress

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
-import Player from '@vimeo/player'
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', onPlay);
+player.on('loaded', updateTime)
+
+function onPlay() {
+    player.getCurrentTime().then(function(seconds) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds)
+});
+}
+function updateTime() {
+    const savedPlaybackTime = localStorage.getItem(LOCALSTORAGE_KEY);
+    console.log(savedPlaybackTime);
+    player.setCurrentTime(savedPlaybackTime).then(function (seconds) {
+    console.log(seconds);
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+        }
+    });
+};
